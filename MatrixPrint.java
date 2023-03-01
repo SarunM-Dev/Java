47692bb406d0df4b4ba709c8399b0f99616768f6
SarunSMS <126680594+SarunSMS@users.noreply.github.com>
2023-03-01 19:12:38 +0530

    Sarun M
    Matrix in 2d array

--- MatrixPrint.java
+++ MatrixPrint.java
@@ -1,26 +1,26 @@
 import java.util.*;
 
-public class MatrixPrint {
+public class MatrixPrint{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner value = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
-        int rows = sc.nextInt();
+        int rows = value.nextInt();
         System.out.print("Enter the number of columns: ");
-        int cols = sc.nextInt();
+        int cols = value.nextInt();
 
         int[][] arr = new int[rows][cols];
 
   	  for (int i = 0; i <arr.length; i++) {
             for (int j = 0; j <arr.length; j++) {
-		arr[i][j]=sc.nextInt();
+		arr[i][j]=value.nextInt();
 }
 }
 
-        for (int i = 0; i <arr.length; i++) {
-            for (int j = 0; j <arr.length; j++) {
-                System.out.print(arr[i][j] + " ");
+     for (int i = 0; i <arr.length; i++) {
+          for (int j = 0; j <arr.length; j++) {
+               System.out.println(arr[i][j] + " ");
            }
             System.out.println();
-        }
-    }
+}
+}
 }
