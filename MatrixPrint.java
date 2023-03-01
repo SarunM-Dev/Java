d667665175c79da7f212f6a4d2b4ee53596be33f
SarunSMS <126680594+SarunSMS@users.noreply.github.com>
2023-03-01 19:23:47 +0530

    Sarun M
    Matrix in 2d array

--- MatrixPrint.java
+++ MatrixPrint.java
@@ -18,7 +18,7 @@ public class MatrixPrint{
 
      for (int i = 0; i <arr.length; i++) {
           for (int j = 0; j <arr.length; j++) {
-               System.out.println(arr[i][j] + " ");
+               System.out.print(arr[i][j] + " ");
            }
             System.out.println();
 }
